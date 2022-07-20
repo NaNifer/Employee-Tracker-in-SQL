dd0f139f5fcfaef6b2b6ccc33ee84ed6f0649d39
Nifer Kilakila <nifercritter@gmail.com>
2022-07-20 11:00:32 -0700

    first commit

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}else
