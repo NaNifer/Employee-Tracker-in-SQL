3d5e0628089e0da9f1fbca94b10d46316841bcbe
Nifer Kilakila <nifercritter@gmail.com>
2022-07-21 09:59:19 -0700

    Seed data fixed

--- model/seed.sql
+++ model/seed.sql
@@ -10,21 +10,21 @@ VALUES ('Customer Service'),
 INSERT INTO role(title, salary, department_id)
 VALUES ('Complaint Listener', 30000, 1),
     ('Head Cat Wrangler', 150000, 1),
+    ('COO', 250000, null),
     ('Accountant', 100000, 5),
     ('Lead Sale Rep', 200000, 3),
     ('Expeditor', 50000, 1),
     ('HR Assistant', 30000, 2),
     ('Content Web Manager', 70000, 4),
-    ('Executive Assistant', 200000, 5),
-    ('COO', 250000, null);
+    ('Executive Assistant', 200000, 5);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ('Red', 'McDonnel', 1, 2),
+VALUES ('Red', 'McDonnel', 1, null),
     ('Anastasia', 'LeGoth', 2, null),
-    ('Muffy', 'Smithsonian', 3, null),
-    ('Chad', 'Oberlin', 4, 9),
-    ('Rhonda', 'Jones', 5, 2),
-    ('Hunter', 'Algiers', 6, 7),
-    ('Nathan', 'Jung', 7, 9),
-    ('Luella', 'Mason', 8, 9),
-    ('Lilou', 'Lighthouse', 9, null);
+    ('Lilou', 'Lighthouse', 3, null),
+    ('Muffy', 'Smithsonian', 4, 1),
+    ('Chad', 'Oberlin', 5, 2),
+    ('Rhonda', 'Jones', 6, 2),
+    ('Hunter', 'Algiers', 7, 1),
+    ('Nathan', 'Jung', 8, 3),
+    ('Luella', 'Mason', 9, 3);
