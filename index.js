2625a81a94ed93047b9ed474cbd77a42bd0dbb03
Nifer Kilakila <nifercritter@gmail.com>
2022-07-25 12:52:05 -0700

    Hot mess, trying to get prompts to work completely.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Packages needed for this application
 const inquirer = require("inquirer");
-const connection = require("./config/connection");
 
 const {
   displayDept,
@@ -37,69 +36,34 @@ async function init() {
           return answers.chooseTask === "Add a department";
         },
       },
-      {
-        type: "input",
-        message: "What is the name of the role?",
-        name: "roleName",
-        when(answers) {
-          answers.chooseTask === "Add a role";
-        },
-      },
-      {
-        type: "input",
-        message: "What is the salary of the role?",
-        name: "roleSalary",
-        when(answers) {
-          answers.chooseTask === "Add a role";
-        },
-      },
-      {
-        type: "input",
-        message: "Which department does the role belong to?",
-        name: "roleDepartment",
-        choices: [
-          // Insert choices from database
-        ],
-        when(answers) {
-          return answers.chooseTask === "Add Role";
-        },
-      },
-      {
-        type: "input",
-        message: "What is the employees first name?",
-        name: "firstName",
-        when(answers) {
-          return answers.chooseTask === "Add an employee";
-        },
-      },
-      {
-        type: "input",
-        message: "What is the employees last name?",
-        name: "lastName",
-        when(answers) {
-          return answers.chooseTask === "Add an employee";
-        },
-      },
-      {
-        type: "input",
-        message: "What is the employees role?",
-        choices: [
-          // Insert ROLE choices from database
-        ],
-        when(answers) {
-          return answers.chooseTask === "Add an employee";
-        },
-      },
-      {
-        type: "input",
-        message: "Who is the employees manager?",
-        choices: [
-          // Insert FULL NAME choices from database
-        ],
-        when(answers) {
-          return answers.chooseTask === "Add an employee";
-        },
-      },
+      // {
+      //   type: "input",
+      //   message: "What is the name of the role?",
+      //   name: "roleName",
+      //   when(answers) {
+      //     answers.chooseTask === "Add a role";
+      //   },
+      // },
+      // {
+      //   type: "input",
+      //   message: "What is the salary of the role?",
+      //   name: "roleSalary",
+      //   when(answers) {
+      //     answers.chooseTask === "Add a role";
+      //   },
+      // },
+      // {
+      //   type: "input",
+      //   message: "Which department does the role belong to?",
+      //   name: "roleDepartment",
+      //   choices: [
+      //     // Insert choices from database
+      //   ],
+      //   when(answers) {
+      //     return answers.chooseTask === "Add Role";
+      //   },
+      // },
+
       {
         type: "input",
         message: "Which employee would you like to update?",
@@ -122,22 +86,22 @@ async function init() {
       },
     ])
     .then((answers) => {
-      console.log(answers.chooseTask);
+      console.log(answers.chooseTask, "line 124 index");
+      console.log(answers, "Line 125 index");
       if (answers.chooseTask === "View all departments") {
-        return displayDept();
+        displayDept();
       } else if (answers.chooseTask === "View all employees") {
-        return displayEmpl();
+        displayEmpl();
       } else if (answers.chooseTask === "Add a department") {
-        return addDept(answers);
+        addDept(answers);
       } else if (answers.chooseTask === "Add a role") {
-        return addRole(answers);
+        addRole(answers);
       } else if (answers.chooseTask === "Add an employee") {
-        return addEmpl(answers);
+        addEmpl(answers);
       } else if (answers.chooseTask === "Update an employee role") {
-        return updateRole(answers);
+        updateRole(answers);
       }
     });
-  init();
 }
 
 // Function call to initialize app
--- util/inquirerFuncts.js
+++ util/inquirerFuncts.js
@@ -1,3 +1,6 @@
+const connection = require("../config/connection");
+const inquirer = require("inquirer");
+
 // Import queries
 const {
   allDptQuery,
@@ -6,7 +9,7 @@ const {
   addRoleQuery,
   addEmplQuery,
   changeRoleQuery,
-} = require("./config/queries");
+} = require("../config/queries");
 
 async function displayDept() {
   try {
@@ -21,7 +24,6 @@ async function displayDept() {
 
 async function displayEmpl() {
   try {
-    console.log("L46", askQ.chooseTask);
     const employees = await connection.query(allEmplQuery);
     console.log("\n\n\n\n\n\nALL EMPLOYEES");
     console.table(employees[0]);
@@ -30,10 +32,92 @@ async function displayEmpl() {
   }
 }
 
-async function addDept() {
+async function addDept(answers) {
   try {
     // POST to DB
-    console.table("\n\n\n\n\nDepartment Added");
+    console.log(`${answers.deptName} Department Added`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addRole(answers) {
+  const addRolePrompt = [
+    {
+      type: "input",
+      message: "What is the name of the role?",
+      name: "roleName",
+    },
+    {
+      type: "input",
+      message: "What is the salary of the role?",
+      name: "roleSalary",
+    },
+    {
+      type: "input",
+      message: "Which department does the role belong to?",
+      name: "roleDepartment",
+      choices: [
+        // Insert choices from database
+      ],
+    },
+  ];
+  inquirer.prompt(addRolePrompt).then((answers) => {
+    try {
+      // POST to DB
+      console.log(
+        `${answers.roleName}, salary ${answers.roleSalary}, has been added to the ${answers.roleDepartment} department.`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
+
+async function addEmpl(answers) {
+  addEmplPrompt = [
+    {
+      type: "input",
+      message: "What is the employees first name?",
+      name: "firstName",
+    },
+    {
+      type: "input",
+      message: "What is the employees last name?",
+      name: "lastName",
+    },
+    {
+      type: "input",
+      message: "What is the employees role?",
+      name: "emplRole",
+      choices: [
+        // Insert ROLE choices from database
+      ],
+    },
+    {
+      type: "input",
+      message: "Who is the employees manager?",
+      name: "emplMgr",
+      choices: [
+        // Insert FULL NAME choices from database
+      ],
+    },
+  ];
+  inquirer.prompt(addEmplPrompt).then((answers) => {
+  try {
+    // POST to DB
+    console.log(
+      `${answers.firstName} ${answers.lastName}, position ${answers.emplRole}, has been added on ${answers.emplMgr}'s team.`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+async function updateRole(answers) {
+  try {
+    // POST to DB
+    console.log("Role Updated");
   } catch (error) {
     console.log(error);
   }
